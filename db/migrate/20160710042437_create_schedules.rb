0f2498e21ae3dc5ef18137d0339ea9d9541f1a91
Xing <sky00000kimo@gmail.com>
2016-07-10 12:40:22 +0800

    nanny schedules migration

--- db/migrate/20160710042437_create_schedules.rb
+++ db/migrate/20160710042437_create_schedules.rb
@@ -1,8 +1,10 @@
 class CreateSchedules < ActiveRecord::Migration[5.0]
   def change
     create_table :schedules do |t|
-
-      t.timestamps
+    	t.references :nanny, foreign_key:true
+    	t.date :date
+    	t.integer :helfhour
+    	t.timestamps
     end
   end
 end
