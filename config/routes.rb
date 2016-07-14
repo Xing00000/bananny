055bc61184be7214baf15c07323d78b8da4e14ae
Xing <sky00000kimo@gmail.com>
2016-07-14 21:06:20 +0800

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users do
-  	resources :nannies
+  	resources :nannies ,only: [:new]
+    resources :parents ,only: [:new]
   end
   resources :nannies do
   	resources :schedules
   end
+  resources :parents
   resources :infos ,only: [:create,:edit,:update]
 
 
