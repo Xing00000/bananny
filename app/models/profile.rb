a7cf37b212386c34112e24da4acc12c6242a9150
Xing <sky00000kimo@gmail.com>
2016-07-14 16:23:57 +0800

    rename female

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ApplicationRecord
 
 	mount_uploader :image, ImageUploader
 
-	GENDER = [:FEMAN,:MAN]
+	GENDER = [:FEMALE,:MALE]
 	CITY = [:台北市]
 	DISTRICT = [:松山區,:信義區,:大安區,:中山區,:中正區,:大同區,:內湖區,:士林區,:北投區,:萬華區,:文山區,:南港區]
 
