8548278e929800776709f89cfffb69401b6bba9f
Xing <sky00000kimo@gmail.com>
2016-07-14 16:24:32 +0800

    nanny search session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	def index
 		if params[:date] != nil && params[:start_time] != nil
+			session["search_nanny"] = params
       @search_nanny = search_nanny(params)
     end
 	end
