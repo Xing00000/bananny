633eeb85ea30974741f72134736e921c25896e51
Xing <sky00000kimo@gmail.com>
2016-07-18 12:25:54 +0800

    image crop

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,7 +32,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  process resize_to_fit: [400, 400]
+  process resize_to_fill: [400, 400]
 
   version :thumb do
     process resize_to_fit: [100, 100]
