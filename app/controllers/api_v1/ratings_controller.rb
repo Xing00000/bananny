f62c98c36805e938ed8a8964a5ee1b0bac30808e
Xing <sky00000kimo@gmail.com>
2016-07-27 19:26:53 +0800

    # api_v1/ratings_controller

--- app/controllers/api_v1/ratings_controller.rb
+++ app/controllers/api_v1/ratings_controller.rb
@@ -2,9 +2,9 @@ class APIV1::RatingsController < APIController
   def rating
     @case = Case.find(params[:id])
     items = @case.items
-    @item = Item.create( :rating_id => ,
-                         :name => params[:name],
-                         :scored => params[:scored] )
+    # @item = Item.create( :rating_id => ,
+    #                      :name => params[:name],
+    #                      :scored => params[:scored] )
     @parent = @case.parent
   end
 end
