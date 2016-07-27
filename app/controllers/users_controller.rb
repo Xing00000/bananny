83f899d2e3662b466591d2116b638842009541e1
Xing <sky00000kimo@gmail.com>
2016-07-27 23:45:14 +0800

    change nanny.info logic to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 		if params[:start_date] != nil && params[:end_date] != nil
 			session[:search_nanny] = params
 			# cookies[:search_nanny] = params
-      @search_nanny = search_nanny(params)
+      @search_nanny = search_nanny(params).select{|n| n.info }
       @start_date = start_date(params)
     end
 	end
