85920b5597f934d5a378574361e72e38cad77586
Xing <sky00000kimo@gmail.com>
2016-07-28 17:32:53 +0800

    cases index view

--- app/models/case.rb
+++ app/models/case.rb
@@ -7,4 +7,14 @@ class Case < ApplicationRecord
   has_many :items, through: :ratings
 
 	has_many :comments
+
+
+	def show_time
+		self.start_date.strftime('%H:%M')+"~"+ self.end_date.strftime('%H:%M')
+	end
+
+	def show_hour
+		(self.end_date - self.start_date)/3600
+	end
+
 end
