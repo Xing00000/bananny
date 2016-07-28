a936c4690802e93ec0e8a10a52bcc386305c4d4f
Xing <sky00000kimo@gmail.com>
2016-07-29 04:32:19 +0800

    fix nanny verification schedule

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
-	def session_or_nil(session)
-		if session
-			return session
-		else
-			""
-		end
+
+	def start_date(session)
+		(session[:date] +" "+ session[:start_date]).to_time
 	end
+
+	def end_date(session)
+		(session[:date] +" "+ params[:end_date]).to_time
+	end
+
+	def count(session)
+		(end_date(session)-start_date(session))/1800
+	end
+
 end
--- app/models/nanny.rb
+++ app/models/nanny.rb
@@ -8,4 +8,23 @@ class Nanny < Profile
 	belongs_to :user
 	has_one :info
 
+
+	def check_schedule?(session)
+		schedules = self.schedules.where("start_date>=? and start_date < ?",start_date(session),end_date(session)).where(:case_id => nil).count
+		schedules == count(session)
+	end
+
+	def start_date(session)
+		(session["date"] + " " + session["start_date"]).to_time
+	end
+
+	def end_date(session)
+		(session["date"] + " " + session["end_date"]).to_time
+	end
+
+	def count(session)
+		(end_date(session)-start_date(session))/1800
+	end
+
+
 end
