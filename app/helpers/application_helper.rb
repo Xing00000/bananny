7ce55e3d2a3bd911048654107d954060c5545291
Xing <sky00000kimo@gmail.com>
2016-07-29 21:56:45 +0800

    index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+	def time_of_experience(time)
+		days = (Time.now.to_date - time.to_date).to_i
+		"#{(days/365).to_s} 年#{(days/30%12).to_s}個月"
+	end
 
+	def baby_age(time)
+		days = (Time.now.to_date - time.to_date).to_i
+		"#{(days/365).to_s} 歲#{(days/30%12).to_s}個月"
+	end
 
 end
