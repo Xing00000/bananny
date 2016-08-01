2ce9b830a6ece1f2c5e7c7a0c364967913ac46f8
Xing <sky00000kimo@gmail.com>
2016-08-02 00:53:18 +0800

    action cable test 3

--- app/assets/javascripts/channels/comments.js
+++ app/assets/javascripts/channels/comments.js
@@ -1,13 +1,14 @@
 var cases;
-
-cases = document.location.pathname.split('/')[1];
-
-if (cases === cases) {
+var case_id;
+cases_html = document.location.pathname.split('/')[1];
+case_id = document.location.pathname.split("/")[2]
+console.log(cases_html);console.log(case_id);
+if (cases_html === "cases" && case_id != null) {
 
 
 App.comments = App.cable.subscriptions.create({
   channel: 'CommentsChannel',
-  case_id: document.location.pathname.split("/")[2]
+  case_id: case_id
 }, {
   connected: function() {},
   disconnected: function() {},
